cce1dd7389e21d97f153fbdd8622e06e070a0df2
Rachele Williams <rachele.williams@hotmail.co.uk>
2019-03-19 19:45:08 +0000

    Improved serverinfo command

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -2,15 +2,25 @@ const Discord = require("discord.js");
 const prefixes = require("./prefix");
 
 module.exports.run = async (bot, message, args) => {
-    let sicon = message.guild.displayAvatarURL
+
+    //Check days
+    function checkDays(date) {
+        let now = new Date();
+        let diff = now.getTime() - date.getTime();
+        let days = Math.floor(diff / 86400000);
+        return days + (days == 1 ? " day" : " days") + " ago";
+    };
     let serverembed = new Discord.RichEmbed()
         .setColor("#FFD700")
-        .setThumbnail(sicon)
+        .setAuthor(message.guild.name, message.guild.iconURL)
         .addField("Server Name", message.guild.name)
-        .addField("Created On", message.guild.createdAt)
-        .addField("You Joined on", message.member.joinedAt)
-        .addField("Total Members", message.guild.memberCount);
-
+        .addField("Server Owner", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`)
+        .addField("Total | Humans | Bots", `${message.guild.members.size} | ${message.guild.members.filter(member => !member.user.bot).size} | ${message.guild.members.filter(member => member.user.bot).size}`, true)
+        .addField("Channels", message.guild.channels.size)
+        .addField("Roles", message.guild.roles.size)
+        .addField("Creation Date", `${message.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.channel.guild.createdAt)})`, true)
+        .setThumbnail(message.guild.iconURL);
+   
         message.channel.send(serverembed);
 }
 

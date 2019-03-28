ec7ff509edbecaa101bc854d1630964a97bb7d94
Rachele Williams <rachele.williams@hotmail.co.uk>
2019-03-28 22:45:35 +0000

    Added shamebell for VC

--- commands/fun/shame.js
+++ commands/fun/shame.js
@@ -1,11 +1,20 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    
-    message.channel.send("🔔 Shame!", {
-        file: "./resources/shame.gif" 
-    });
 
+    message.channel.send("🔔 Shame!", {
+        file: "./resources/shame.gif"
+    }); 
+
+    var VC = message.member.voiceChannel;
+
+    if (!VC) return 
+    VC.join()
+        .then(connection => {
+            const dispatcher = connection.playFile('resources/shamebell.mp3');
+            dispatcher.on("end", end => { VC.leave() });
+        })
+        .catch(console.error);
 
 }
 module.exports.help = {

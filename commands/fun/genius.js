2b15f25d6c865000fa496628e69f85ee407346a7
Rachele Williams <rachele.williams@hotmail.co.uk>
2019-03-28 22:50:31 +0000

    Added opus

--- commands/fun/genius.js
+++ commands/fun/genius.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     
-    message.channel.send("What a genius", {
+    message.channel.send("Genius!", {
         file: "./resources/genius.gif" 
     });
 
